237d7cb3f3f82914cea7fe5173c4c19664fea0a9
Nach1n <l.ancavilpino@gmail.com>
2024-03-09 13:59:58 -0300

    fix(*): Fix typo on insert method

--- BST-V1.py
+++ BST-V1.py
@@ -17,7 +17,7 @@ class TreeNode:
             if not self.right:
                 self.right = TreeNode(value)
             else:
-                self.right.inser(value)
+                self.right.insert(value)
 
     def find(self, value):
         if value < self.value:
@@ -32,3 +32,14 @@ class TreeNode:
                 self.right.find(value)
         else:
             return True
+
+
+tree = TreeNode(5)
+tree.insert(4)
+tree.insert(6)
+tree.insert(7)
+tree.insert(2)
+tree.insert(1)
+tree.insert(15)
+
+print(tree.left.left.left.value)
